55788fd172826f68df9a6540fd4c6b9aeef93bd8
D4N13l4002 <darriagaa2@unemi.edu.ec>
2025-10-20 02:00:19 -0500

    primer commit

--- EduVision/urls.py
+++ EduVision/urls.py
@@ -1,32 +1,21 @@
-"""
-URL configuration for EduVision project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls_vision")),        
-    path("api/", include("api.urls_flashcards")),    
-]
 
+    # Endpoints de visión
+    path("api/vision/", include("api.urls_vision")),
+
+    # Endpoints de flashcards
+    path("api/flashcards/", include("api.urls_flashcards")),
+
+    # Si tienes otros módulos, agrégalos aquí igual:
+    # path("api/object-game/", include("api.urls_object_game")),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    
