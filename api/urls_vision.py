f9db4c1e2c9dbc29e7e67abf04e5b0f944d88c27
D4N13l4002 <darriagaa2@unemi.edu.ec>
2025-10-20 03:01:14 -0500

    Actualización de rutas y botón de flashcards

--- api/urls_vision.py
+++ api/urls_vision.py
@@ -5,10 +5,10 @@ from . import views_vision
 
 
 urlpatterns = [
-    path("vision/live/", views_vision.live_view, name="live_view"),
-    path("vision/start/", views_vision.start_camera_view, name="start_camera"),
-    path("vision/stop/", views_vision.stop_camera_view, name="stop_camera"),
-    path("vision/objects/", views_vision.objects_view, name="objects_view"),
-    path("vision/frame/", views_vision.get_frame_view, name="frame_view"),  # 👈 ESTE ENDPOINT ES CLAVE
-    path("vision/detect/", views_vision.DetectView.as_view(), name="detect"),
+    path("live/", views_vision.live_view, name="live_view"),
+    path("start/", views_vision.start_camera_view, name="start_camera"),
+    path("stop/", views_vision.stop_camera_view, name="stop_camera"),
+    path("objects/", views_vision.objects_view, name="objects_view"),
+    path("frame/", views_vision.get_frame_view, name="frame_view"),
+    path("detect/", views_vision.DetectView.as_view(), name="detect"),
 ]
